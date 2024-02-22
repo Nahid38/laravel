d75e070dc2887a44cbd8d61cc5d07799c1cbc5a9
Nahid Hossen <nahid957hossen@gmail.com>
2024-02-22 23:43:26 +0600

    [Middleware] Apply For Whole Application

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,8 @@ use App\Http\Middleware\demo;
 use Illuminate\Support\Facades\Route;
 
 
-Route::middleware(['demos'])->group(function (){
+
     Route::get("/hello1/{key}", [DemoController::class,'demoAction1']);
     Route::get("/hello2/{key}", [DemoController::class,'demoAction2']);
     Route::get("/hello3/{key}", [DemoController::class,'demoAction3']);
     Route::get("/hello4/{key}", [DemoController::class,'demoAction4']);
-});

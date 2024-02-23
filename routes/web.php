dca904d2a026938803b3f7f423e97cfb95ec098f
Nahid Hossen <nahid957hossen@gmail.com>
2024-02-23 23:30:40 +0600

    [Controller] Single Action Controller

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
 use App\Http\Controllers\DemoController;
+use App\Http\Controllers\VController;
 use App\Http\Middleware\demo;
 use Illuminate\Support\Facades\Route;
 
 
 
     Route::get("/hello", [DemoController::class,'demoAction']);
+    Route::get("/video", VController::class);

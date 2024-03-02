ede138665e346c0e9c68cdbd6624d2aca6398ab7
Nahid Hossen <nahid957hossen@gmail.com>
2024-03-02 23:48:41 +0600

    new colum

--- database/migrations/2024_02_28_174419_create_profiles.php
+++ database/migrations/2024_02_28_174419_create_profiles.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
-            $table->();
+            $table->string('name');
             $table->timestamps();
         });
     }

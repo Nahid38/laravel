46caf3566c0e29ea1b01441953d4798db66dbaa9
Nahid Hossen <nahid957hossen@gmail.com>
2024-03-11 21:45:13 +0600

    update

--- database/migrations/2024_02_28_174419_create_profiles.php
+++ database/migrations/2024_02_28_174419_create_profiles.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->timestamps('years');
             $table->string('name');
+            $table->string('name');
         });
     }
 
